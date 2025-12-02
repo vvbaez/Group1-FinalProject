574500778eb779a371f263eaa401c0750596ec41
vvbaez <vanemarin101@gmail.com>
2025-12-01 23:27:44 -0500

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -5,6 +5,19 @@
 #include "MedicalInformation.h"
 
 class Doctor : public MedicalInformation {
+private :
+string name;
+string specialty;
+int password;
+
+public : 
+Doctor ();
+Doctor (string name, string specialty, int password); //doctor constructor w real info
+
+void viewPatient(); //doctor reads patient info
+void updatePatientRecord(); //doctor updates info
+
 
 
 #endif
+
