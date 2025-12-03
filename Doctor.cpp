9906ccdb48c48c83751fa007ad82d1366bea7c39
vvbaez <vanemarin101@gmail.com>
2025-12-03 02:29:49 -0500

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,2 +1,57 @@
 // Doctor.cpp
+
 #include "Doctor.h"
+#include <iostream> 
+using namespace std;
+
+Doctor::Doctor() {
+    name = "";
+    specialty = "";
+    password = 0;
+} 
+
+Doctor::Doctor(string n, string s, int p) {
+    name = n;
+    specialty = s;
+    password = p;
+}
+
+void Doctor::displayInfo() {
+    cout << "Doctor Information";
+    cout << "Name: " << name << endl;
+    cout << "Specialty: " << specialty << endl;}
+
+void Doctor::viewPatient() {
+    string birth;
+   cout << "Enter the patient's DOB you want to view: ";
+    cin >> birth;
+
+    getInformation(birth);}
+
+void Doctor::updatePatientRecord() {
+    string birth;
+    string newInfo;
+
+    cout << "Enter patient's DOB you want to update: ";
+    cin >> birth;
+
+    cout << "Enter the updated information: ";
+    cin.ignore();
+    getline(cin, newInfo);
+
+    modifyInformation(birth, newInfo);}
+
+bool Doctor::operator==(const Doctor& other) const {
+    return name == other.name && specialty == other.specialty;}
+
+ostream& operator<<(ostream& out, const Doctor& d) {
+    out << "Doctor: " << d.name << " (" << d.specialty << ")";
+    return out;}
+
+string Doctor::getName() const {
+    return name;}
+
+string Doctor::getSpecialty() const {
+    return specialty;}
+
+
